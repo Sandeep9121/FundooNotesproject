1dc70e985b88415031ac32342abe95910d5c5de2
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-02-25 17:23:18 +0530

    [Sandeep].add UserRegistration

---
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/FundooNotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.bridzelabz.fundoonotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooNotesApiApplication.class, args);
+	}
+
+}
---
+++ FundooNotesApi/src/test/java/com/bridzelabz/fundoonotes/FundooNotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridzelabz.fundoonotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooNotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
