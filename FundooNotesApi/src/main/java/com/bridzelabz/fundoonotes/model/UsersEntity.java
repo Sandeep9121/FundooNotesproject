ee315948cd0558303159be51058c6bb4e7a7daa2
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-02-26 16:28:57 +0530

    userRegister

--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/model/UsersEntity.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/model/UsersEntity.java
@@ -11,111 +11,31 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
+
+@Component
 @Data
 @Entity
-@Table(name="Userdata")
+@Table(name = "Userdata")
 public class UsersEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private long userId;
-	
-	
-	 @Column(name = "user_name",nullable = false )
-	 
-	 @NotEmpty(message = "Please provide an username")
-	 
+
 	private String name;
-	
-	
-	 @Column(name = "password")
-	 
-	 @Size(min = 5)
-	 
-	 @NotEmpty(message = "Please provide password")
-	 
+
+	private String email;
+
 	private String password;
 	
+	private Long mobile_number;
 
-	 @Column(name = "email", nullable = false, unique = true)
-	 
-	 @NotEmpty(message = "Please provide an e-mail")
-	
-	private String email;
-	
-	public long getUserId() {
-		return userId;
-	}
+	private LocalDateTime date;
 
-	public void setUserId(long userId) {
-		this.userId = userId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public long getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(long mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-
-	public LocalDateTime getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDateTime date) {
-		this.date = date;
-	}
-
-	public boolean isVerified() {
-		return isVerified;
-	}
-
-	public void setVerified(boolean isVerified) {
-		this.isVerified = isVerified;
-	}
-
-	
-	 @Column(name = "mobileNumber" ,nullable = false )
-	 
-	  @Size(min =10 ,max = 12)
-	  
-	  @NotEmpty(message = "Please provide an mobileNumber")
-	 
-	private long mobileNumber;
-	
-	
-	 @Column(name = "Date")
-	 	private LocalDateTime date;
-	
-	
-	@Column(name = "verify", columnDefinition = "boolean default false", nullable
-	  = false)
-	 	private boolean isVerified;
+	private boolean isVerified;
 
 }
--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/services/ServiceImplementation.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/services/ServiceImplementation.java
@@ -1,20 +1,48 @@
 package com.bridzelabz.fundoonotes.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.bridzelabz.fundoonotes.dto.UsersDto;
 import com.bridzelabz.fundoonotes.model.UsersEntity;
+import com.bridzelabz.fundoonotes.repository.UsersRepository;
 
-public class ServiceImplementation implements UsersServices{
+@Service
+public class ServiceImplementation implements UsersServices {
+	@Autowired
+	private UsersEntity user;
+	@Autowired
+	private UsersRepository userRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper; 
+	
+	@Autowired
+	private BCryptPasswordEncoder encryptPass;
 
+	
 	
 	@Override
-	public void addUsers() {
-		// TODO Auto-generated method stub
-		
+	@Transactional
+	public void addUsers(UsersDto usersdto) {
+         user=modelMapper.map(usersdto, UsersEntity.class);
+         user.setName(usersdto.getName());
+         user.setEmail(usersdto.getEmail());
+         user.setPassword(encryptPass.encode(usersdto.getPassword()));
+         user.setMobile_number(usersdto.getMobile_number());
+         user.setDate(LocalDateTime.now());
+         
+		userRepository.save(user);
+
 	}
-	
-	
 	@Override
 	public List<UsersEntity> getUserDetails() {
 		// TODO Auto-generated method stub
@@ -32,7 +60,6 @@ public class ServiceImplementation implements UsersServices{
 		// TODO Auto-generated method stub
 		return false;
 	}
-
 
 
 }
