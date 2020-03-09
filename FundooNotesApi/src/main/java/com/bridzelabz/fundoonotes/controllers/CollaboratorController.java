90a37342f60bdbdd383babf0d4ec75abb4b85cc8
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-03-09 11:32:47 +0530

    [Sandeep].Refactored collaborator controller

--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/controllers/CollaboratorController.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/controllers/CollaboratorController.java
@@ -1,6 +1,7 @@
 package com.bridzelabz.fundoonotes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridzelabz.fundoonotes.model.NotesEntity;
 import com.bridzelabz.fundoonotes.reponse.Response;
 import com.bridzelabz.fundoonotes.services.ICollaboratorServices;
 
@@ -19,9 +21,14 @@ private ICollaboratorServices serviceCollaborator;
 
 
 @PostMapping("/addCollaborator")
-public ResponseEntity<Response> addCollaborator(@RequestParam("notesId") Long notesid,
+public ResponseEntity<Response> addCollaborator(@RequestParam("notesId") Long notesId,
 		@RequestParam("email") String email, @RequestHeader("token") String token){
-		return null;
+	NotesEntity note=serviceCollaborator.addCollaborator(notesId, email, token);
+	 if(note!=null) {
+		 return ResponseEntity.status(HttpStatus.CREATED).body(new Response("Collaborator is Added"));
+	 }
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(new Response("Collaborator is not added"));
 	
 }
 }
