cdd16b570f3f267d138b78fd4859a71a2128c387
Sandeep9121 <sandeep.rayala14@gmial.com>
2020-03-11 17:57:59 +0530

    [Sandeep].add profile pic

--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/model/ProfileEntity.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/model/ProfileEntity.java
@@ -1,16 +1,21 @@
 package com.bridzelabz.fundoonotes.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "profile")
+
 public class ProfileEntity {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
