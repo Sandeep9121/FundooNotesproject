621d5788bfb150d1616e598c66e8e3b9657992a8
Sandeep9121 <sandeep.rayala14@gmial.com>
2020-03-11 16:04:49 +0530

    [Sandeep].add  added getallnotesCollborators service

--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/services/ICollaboratorServices.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/services/ICollaboratorServices.java
@@ -1,9 +1,16 @@
 package com.bridzelabz.fundoonotes.services;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.bridzelabz.fundoonotes.model.NotesEntity;
 
 public interface ICollaboratorServices {
 	NotesEntity addCollaborator(Long notesId,String email,String token);
 	
 	NotesEntity removeCollaborator(Long notesId,String email,String token);
+	
+	List<NotesEntity> getAllNotesCollaborators(String token) throws JWTVerificationException,
+	IllegalArgumentException,UnsupportedEncodingException;
 }
