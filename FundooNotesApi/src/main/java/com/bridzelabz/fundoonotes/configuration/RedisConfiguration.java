12ef5e3223d7120f4fe7db20394825d3c494c693
amarvind16116 <amarvind16116@gmail.com>
2020-03-13 11:02:28 +0530

    [Sandeep].add the Redis cache added to notesimplementation

--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/configuration/RedisConfiguration.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/configuration/RedisConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfiguration {
 	   @Bean
 	    JedisConnectionFactory jedisConnectionFactory() {
+		  // redisStandaloneConfiguration.setPassword(RedisPassword.of("password"));
 			return  new JedisConnectionFactory(); 
 	   }
 	   @Bean
@@ -20,17 +21,3 @@ public class RedisConfiguration {
 	    }
 	   
 }
-/*  RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-	        redisStandaloneConfiguration.setPassword(RedisPassword.of("password"));
-	        return new JedisConnectionFactory(redisStandaloneConfiguration);
-	    }
-
-	   
-}*/
-	 /*   @Bean
-	    public RedisTemplate redisTemplate() {
-	        RedisTemplate template = new RedisTemplate<>();
-	        template.setConnectionFactory(jedisConnectionFactory());
-	        return template;
-	    }
-	}*/
