7d72c64437e0e89db664d8324e3a302fa195714b
Sandeep9121 <sandeep.rayala14@gmail.com>
2020-03-13 15:43:17 +0530

    [Sandeep].add Rabbit Mq

--- FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/services/UserImplementation.java
+++ FundooNotesApi/src/main/java/com/bridzelabz/fundoonotes/services/UserImplementation.java
@@ -37,6 +37,8 @@ import lombok.extern.slf4j.Slf4j;
 public class UserImplementation implements IUsersServices {
 
 	@Autowired
+	
+	
 	private EntityManager entityManager;
 	private UsersEntity user = new UsersEntity();
 	@Autowired
